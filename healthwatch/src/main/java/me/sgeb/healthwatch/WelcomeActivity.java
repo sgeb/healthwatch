6dda930ede5d44fd8e400b9fc579d4de304b1cd0
Serge Gebhardt <serge.gebhardt@gmail.com>
2014-01-27 14:52:09 -0200

    Add auth callback activity

--- healthwatch/src/main/java/me/sgeb/healthwatch/WelcomeActivity.java
+++ healthwatch/src/main/java/me/sgeb/healthwatch/WelcomeActivity.java
@@ -1,7 +1,10 @@
 package me.sgeb.healthwatch;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 
 public class WelcomeActivity extends Activity {
 
@@ -11,4 +14,10 @@ public class WelcomeActivity extends Activity {
         setContentView(R.layout.activity_welcome);
     }
 
+    public void authorize(View view) {
+        Uri intentUri = Uri.parse("healthwatch://localhost/authcallback");
+        Intent intent = new Intent(Intent.ACTION_VIEW, intentUri);
+        intent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP | Intent.FLAG_ACTIVITY_NO_HISTORY);
+        startActivity(intent);
+    }
 }
