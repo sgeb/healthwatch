b3976aacc8013294832119b9f5b9d5c4ea7e1b6b
Serge Gebhardt <serge.gebhardt@gmail.com>
2014-01-28 22:18:32 -0200

    Add comment to avoid committing sensitive information

--- healthwatch/src/main/java/me/sgeb/healthwatch/hgclient/AuthKeys.java
+++ healthwatch/src/main/java/me/sgeb/healthwatch/hgclient/AuthKeys.java
@@ -3,6 +3,11 @@ package me.sgeb.healthwatch.hgclient;
 public class AuthKeys {
     // Register your app and fill in with your own application keys
     // See https://runkeeper.com/partner/applications/
+
+    // Once you filled in the keys, make sure to run
+    //   git update-index --assume-unchanged AuthKeys.java
+    // to avoid accidentally committing sensitive information.
+
     private final String CLIENT_ID = "fill in with your details";
     private final String CLIENT_SECRET = "fill in with your details";
 
