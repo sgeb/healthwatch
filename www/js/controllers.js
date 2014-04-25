edf0eebbfcec4cfa4b8e48d1ed3bd3b71aa1107c
Serge Gebhardt <serge.gebhardt@gmail.com>
2014-04-25 10:36:02 -0300

    Adjust to healthwatch project, add runkeeper auth

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -3,6 +3,18 @@ angular.module('starter.controllers', [])
 .controller('AppCtrl', function($scope) {
 })
 
+.controller('BrowseCtrl', function($scope) {
+  $scope.oauth = function() {
+    // replace with your oewn oauth.io public key
+    OAuth.initialize('nIz5pP6_YPsuE0CrnNJZdGTScgM');
+    OAuth.popup('runkeeper', function(err, result) {
+      //handle error with err
+      //use result.access_token in your API request
+      alert('asdf: ' + result.access_token);
+    });
+  }
+})
+
 .controller('PlaylistsCtrl', function($scope) {
   $scope.playlists = [
     { title: 'Reggae', id: 1 },
